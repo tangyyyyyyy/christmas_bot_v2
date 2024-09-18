e6bdbe3f204527ac5b70a929b1d4ffead307ac2a
Janina Alvarez <janinatalvarez@ucla.edu>
2024-09-18 00:38:18 -0700

    remove some spaces

--- server/schemas/creature.js
+++ server/schemas/creature.js
@@ -8,35 +8,29 @@ const creatureSchema = new Schema({
     required: true,
     unique: true,
   },
-
   pronoun: {
     type: String,
     required: true,
   },
-
   items: [
     {
       type: itemSchema,
       required: true,
     },
   ],
-
   nature: {
     type: String,
     required: true,
     validator: (value) => value in ['naughty', 'nice'],
   },
-
   image: {
     type: String,
     default: '',
   },
-
   isFound: {
     type: Boolean,
     default: false,
   },
-
   foundBy: {
     type: playerSchema,
   },
