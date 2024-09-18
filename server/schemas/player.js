57aad59f959fd4c2a3566852f443506573710193
kashyap <kashyap.ganesan@gmail.com>
2024-09-17 19:15:44 -0700

    resolve merge conflict AGAIN

--- server/schemas/player.js
+++ server/schemas/player.js
@@ -10,7 +10,7 @@ const playerSchema = new Schema({
     serverId: {
         type: Number,
         required: true,
-        immutable: true,
+        immutable: true
     },
     score: {
         type: Number,
